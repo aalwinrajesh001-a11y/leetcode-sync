ceb9242e5363d0dafdaf2966e8d9f2b7137b4580
aalwinrajesh001-a11y <aalwinrajesh@gmail.com>
2026-07-09 22:17:40 +0530

    Improved Input Error Handling

--- input_handler.py
+++ input_handler.py
@@ -1,21 +1,50 @@
+languages={
+        "python":"py",
+        "py":"py",
+        "cpp":"cpp",
+        "c++":"cpp",
+        "c plus plus":"cpp",
+        "java":"java",
+        "c":"c"
+        }
+
+topics=['ARRAY', 'STRINGS', 'LINKED LIST', 'TREE', 'GRAPH', 'DYNAMIC PROGRAMMING', 'HASH TABLE']
+
 def basic_details():
 
-	p=input("\tPROBLEM NAME : ")
-	t=input("\tPROBLEM TOPIC : ")
-	l=input("\tLANGUAGE : ")
-	return p,t,l
+	problem_name=input("\tPROBLEM NAME : ")
+	while problem _name=="" or problem_name==" ":
+		problem_name=input("\tNAME CANNOT BE EMPTY. PLEASE ENTER A VALID NAME : ")
+
+	topic=input(f"\tPROBLEM TOPIC\n{topics} : ").upper()
+	while topic not in topics:
+		topic=input(f"\tINVALID ! PLEASE SELECT FROM THE LSIT \n\t{topics} : ").upper()
+	
+	language=input("\tLANGUAGE (PYTHON/CPP/JAVA) : ")
+
+	while language not in languages:
+		language=input("INVALID !\nENTER A VALID LANGUAGE : ")
+	
+	return problem_name,topic,language
 
 def input_code():
-	
+
 	print("\n\nEnter the copied code here (Type 'END' in a new line when finished) : \n")
+	code=""
 	lines=[]
 	while True:
         	line=input()
-        	if line=='END':
+        	if line.lower()=='end':
                 	break
 	        lines.append(line)
 	code="\n".join(lines)
-	return code,len(lines)
+	n=len(lines)
 
-def check_details(p,t,l,c):
-        print(f"\n\nDETAILS :\n\t PROBLEM NAME :{p}\n\t TOPIC : {t}\n\t LANGUAGE : {l}\n\t CODE : \n{c} ")
+	if code=="":
+		print("\n\nERROR! CODE CANNOT BE EMPTY ")
+		code,n=input_code()
+
+	return code,n
+
+def check_details(name,topic,language,code):
+        print(f"\n\nDETAILS :\n\t PROBLEM NAME :{name}\n\t TOPIC : {topic}\n\t LANGUAGE : {language}\n\t CODE : \n{code} ")
