9669f4e5028c5f94f1ffd90ae260cef815861769
aalwinrajesh001-a11y <aalwinrajesh@gmail.com>
2026-07-09 22:21:54 +0530

    Error handling implimented while git operations

--- git_handler.py
+++ git_handler.py
@@ -1,12 +1,23 @@
 import subprocess
 
-def git_push(problem):
-	commit_msg="SOLVED : "+problem
-	print("\nADDING FILE...")
-	subprocess.run(['git','add','.'])
-	print("\nCOMMITING...")
-	subprocess.run(['git','commit','-m',commit_msg])
-	print("\nPUSHING TO GIT HUB...")
-	subprocess.run(['git','push'])
-	print("\n\tSUCCESFULLY PUSHED.\n")
-	return
+def git_push(problem_name):
+	try:
+		commit_msg="SOLVED : "+problem_name
+		print("\nADDING FILE...")
+		subprocess.run(['git','add','.'],capture_output=True,text=True,check=True)
+		print("\nFILE STAGED SUCCESSFULLY.")
+
+		print("\nCOMMITING...")
+		subprocess.run(['git','commit','-m',commit_msg],capture_output=True,text=True,check=True))
+		print(\nf"SUCCESSFULLY COMMITED WITH COMMIT MESSAGE : '{commit_msg}' ")
+
+		print("\nPUSHING TO GIT HUB...")
+		subprocess.run(['git','push'],capture_output=True,text=True,check=True))
+		print("\n\tSUCCESFULLY PUSHED.\n")
+	except subprocess.CalledProcessError as e:
+		print("\nAUTOMATION FAILED !")
+		print(f"FAILED DURING EXCECUTION OF : {" ".join(e.cmd)}")
+		print(f"\nEXIT CODE :{e.returncode}")
+		print(f"\n ERROR  DETAILS :\n{e.stderr.strip()}")
+		return False
+	return True
